a23164404a6c51980a9fb3b12d0576669164df45
Jan <mail@jantmueller.com>
2023-10-08 20:53:41 +0200

    added valid user

--- extensions/minecraft_server.py
+++ extensions/minecraft_server.py
@@ -1,8 +1,18 @@
 import lightbulb
 import psutil
 import subprocess
+import os
 
 plugin = lightbulb.Plugin("minecraft_server")
+valid_user_ids = eval(os.getenv('VALID_USER_IDS'))
+
+def valid_user(func):
+    async def wrapper(ctx):
+        if ctx.author.id in valid_user_ids:
+            return func(ctx)
+        else:
+            await ctx.respond("You are not authorized to use this command.")
+    return wrapper
 
 @plugin.command()
 @lightbulb.command("start-server", "Starts the Minecraft server.", auto_defer=True)
@@ -17,6 +27,7 @@ async def start_server(ctx):
 @plugin.command()
 @lightbulb.command("server-online", "Checks if the server is online.", auto_defer=True)
 @lightbulb.implements(lightbulb.SlashCommand)
+@valid_user
 async def server_online(ctx):
     if is_server_running():
         await ctx.respond("Server is online.")
@@ -28,6 +39,9 @@ def is_server_running():
         if "java" in process.info["cmdline"] and 'server.jar' in process.info["cmdline"]:
             return True
     return False
+
+
+    
 
 def load(bot):
     bot.add_plugin(plugin)
